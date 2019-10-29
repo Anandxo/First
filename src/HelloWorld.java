8aac81a6b3f2ae645a30dc5c5d1a699f6a44e674
mranand <mranand@mranands-MacBook-Pro.local>
2019-10-29 15:28:17 +0530

    Add . test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String Args[]){
         System.out.printf("Hello world");
+        Test test = new Test();
+        test.print();
     }
 }
+
---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    void print()
+    {
+        System.out.println("test");
+    }
+}
